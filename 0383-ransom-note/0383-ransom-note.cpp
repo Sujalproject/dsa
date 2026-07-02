ffb6cbcf720a2733ae7df6ebec7ceb43715ba080
Sujalproject <sujalkumarpatwa1@gmail.com>
2026-07-02 23:49:52 +0530

    Time: 4 ms (39.95%), Space: 11.9 MB (49.11%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<magazine.size();i++){
+            mp[magazine[i]]++;
+        }
+        for(int i=0;i<ransomNote.size();i++){
+            if(mp.find(ransomNote[i])==mp.end()){
+                return false;
+            }
+            if(mp[ransomNote[i]]){
+                mp[ransomNote[i]]--;
+                if(mp[ransomNote[i]]==0) mp.erase(ransomNote[i]);
+                continue;
+            }
+        }
+        return true;
+    }
+};
